63f19df2ec7d3ecf00719aee8795c745b4bbba15
Roger Sosa <RSpro@Rogers-MacBook-Pro.local>
2019-03-07 14:26:34 -0600

    Librerias

--- Light_1/Light_1.ino
+++ Light_1/Light_1.ino
@@ -1,3 +1,9 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+
 void setup() {
   // put your setup code here, to run once:
 
